1f1c6048888dc653cbb17924500e623f3bc8cd26
zhang96 <zhang96@users.noreply.github.com>
2016-02-05 13:51:48 -0500

    Init

---
+++ Warmer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Warmer
+//
+//  Created by Jerry Zhang on 2016-02-05.
+//  Copyright © 2016 Jerry Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
